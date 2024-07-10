cb936f8fc52b59289c64899b13ab05aaac326dc3
Guillaume Claverie <guillaume.claverie@mail.com>
2024-07-10 16:26:03 +0200

    deps: add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "~/modules/vueCodeBlocModule"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "~/modules/vueCodeBlocModule",
+    "@pinia/nuxt"
+  ],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
