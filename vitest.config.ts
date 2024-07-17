82f5a1aa3f6a5cb782bfd7c175aab9c529cadbaa
Guillaume Claverie <guillaume.claverie@mail.com>
2024-07-17 15:17:21 +0200

    tests: add jsdom

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,4 +2,8 @@ import { defineVitestConfig } from "@nuxt/test-utils/config";
 
 export default defineVitestConfig({
   // any custom Vitest config you require
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
 });
