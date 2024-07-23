e68fb24eb2bd177155c44ada0ea506ad8c12ee02
Guillaume Claverie <guillaume.claverie@mail.com>
2024-07-23 14:27:13 +0200

    refactor: schemaStore use directly schemaBuilder

--- composables/stores/schemaStore.ts
+++ composables/stores/schemaStore.ts
@@ -1,16 +1,8 @@
 import { defineStore } from "pinia";
 import { useSchemaBuilder } from "../useDynamicSchemaBuilder";
-import type { SchemaField } from "../useDynamicSchemaBuilder";
 
 export const useSchemaStore = defineStore("schemaStore", () => {
   const schemaBuilder = useSchemaBuilder();
 
-  function addField(field: SchemaField) {
-    schemaBuilder.addField(field);
-  }
-
-  return {
-    ...schemaBuilder,
-    addField,
-  };
+  return schemaBuilder;
 });
